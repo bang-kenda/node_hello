bd765bfb20432a0a137ddcf1136af15d099c51d2
bang <bangkenda1234@gmail.com>
2025-03-09 22:24:16 +0700

    use template engine handlebars

---
+++ src/index.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const hbs     = require('express-handlebars')
+const morgan  = require('morgan')  // morgan lấy các thông tin request để lập trình viên dễ quan sát trong quá trình thực hiện
+const app     = express()
+const port    = 3000
+
+
+app.use(morgan('combined'));
+
+//template engine
+app.engine('hbs', hbs.engine({ // ở đây thì đặt lại tên đuôi của views
+  extname : 'hbsl'              // đặt lại tên đuôi cho file layout là hbsl (đặt là hbs cũng được)
+                                // đặt đuôi khác tý để hiểu hơn về các file
+}));
+app.set('view engine', 'hbs');
+app.set('views', 'src/views');
+
+app.get('/', (req, res) => {
+  // res.send('Hello World nha ban nhung!')
+  res.render('home');
+})
+
+app.get('/news', (req, res) => {
+  res.render('news');
+})
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
