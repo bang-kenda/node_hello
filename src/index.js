e18c5bf6a140b8cff29144fc93df843c4cb24fcf
bang <bangkenda1234@gmail.com>
2025-03-11 23:12:14 +0700

    query parameters get post

--- src/index.js
+++ src/index.js
@@ -17,15 +17,20 @@ app.engine('hbs', hbs.engine({ // ở đây thì đặt lại tên đuôi của 
 app.set('view engine', 'hbs');
 app.set('views', 'src/views');
 
+//routing
 app.get('/', (req, res) => {
   // res.send('Hello World nha ban nhung!')
-  res.render('home');
+  res.render('home'); // đi tìm cái view tên là home để hiển thị
 })
-
+//routing
 app.get('/news', (req, res) => {
-  res.render('news');
+  console.log(req.query.q); // xem giá trị của req 
+  res.render('news'); // đi tìm cái view tên là news để hiển thị
 })
-
+//routing
+app.get('/search', (req, res) => {
+  res.render('search'); // đi tìm cái view tên là search để hiển thị
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
